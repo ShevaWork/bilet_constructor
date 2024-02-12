47952e5e49375a4fe44e05230b907c574b0520d0
ShevaWork <101905253+ShevaWork@users.noreply.github.com>
2024-02-12 15:57:45 +0200

    Delete getShablon.php

--- getShablon.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-$mysql = new mysqli('localhost','root','root','create');
-$result = $mysql-> query("SELECT * FROM `base_shablons` WHERE `email` = '$email' AND `name`= '$name'");
-$shablon = $result->fetch_assoc();
-echo json_encode($shablon);
